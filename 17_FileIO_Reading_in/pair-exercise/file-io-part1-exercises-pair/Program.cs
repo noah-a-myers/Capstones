7912f8f3a77eb7b58f2ad7954580b13f3fd7eabe
John Fulton <john@techelevator.com>
2019-06-03 16:40:37 -0400

    17_FileIO_Reading_in/

---
+++ 17_FileIO_Reading_in/pair-exercise/file-io-part1-exercises-pair/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace file_io_part1_exercises_pair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
