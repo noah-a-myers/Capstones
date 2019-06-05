6f347507198971245f6c9ed6073e7d06efbf3b5e
Jason Homan <homan.jason12@gmail.com>
2019-06-05 14:05:20 -0400

    finish

--- 17_FileIO_Reading_in/pair-exercise/file-io-part1-exercises-pair/Program.cs
+++ 17_FileIO_Reading_in/pair-exercise/file-io-part1-exercises-pair/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace file_io_part1_exercises_pair
 {
@@ -6,7 +7,35 @@ namespace file_io_part1_exercises_pair
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a file system path");
+            string path = Console.ReadLine();
+
+            int wordCount = 0;
+            int sentenceCount = 0;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] words = line.Split(' ');
+                    wordCount += words.Length;
+                }
+            }
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    char character = (char)sr.Read();
+                    if (character == '.' || character == '?' || character == '!')
+                    {
+                        sentenceCount++;
+                    }
+                }
+            }
+            Console.WriteLine("word count = " + wordCount);
+            Console.WriteLine("sentence count = " + sentenceCount);
+            Console.Read();
         }
     }
 }
