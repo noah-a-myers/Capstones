ad16fa2ab4018d7b35b5d0018030a32ed0cd5db5
John Fulton <john@techelevator.com>
2019-06-05 08:32:11 -0400

    18_FileIO_Writing_out/

---
+++ 18_FileIO_Writing_out/pair-exercise/FindAndReplace/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindAndReplace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
